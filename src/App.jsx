5d185452acaef4e028b70ee9729df1ea39c5a12e
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-12-30 19:23:12 +0600

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
 
-      <h1 className='text-3xl text-red-500'>Bismilllah hir rahmanir arahimm</h1>
+      <h1 className='text-3xl text-red-500'>Bismilllah hir rahmanir arahimsdasm</h1>
    
 
     </>
